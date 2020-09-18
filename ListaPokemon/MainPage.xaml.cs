04ed1c37f41a9f177b8e4635fe45945783471fca
Lucas Pinto <lucas.pinto@dxc.com>
2020-09-18 00:38:03 -0300

    first commit Lista Pokemon

---
+++ ListaPokemon/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using System.Collections.ObjectModel;
+
+namespace ListaPokemon
+{
+    public partial class MainPage : ContentPage
+    {
+        internal ObservableCollection<Pokemon> Pokemons = new ObservableCollection<Pokemon>
+        {
+            new Pokemon
+            {
+                Imagem = "abra.png",
+                Nome="Abra",
+                Tipo="Psi"
+            },
+            new Pokemon
+            {
+                Imagem="alakazam",
+                Nome="alalazam",
+                Tipo="Psi"
+            },
+            new Pokemon
+            {
+                Imagem="charmander.png",
+                Nome="Charmander",
+                Tipo="Fogo"
+            },
+        };
+        public MainPage()
+        {
+            InitializeComponent();
+            lstPokemon.ItemsSource = Pokemons;
+        }
+
+        private void OnSelection(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            if(e.SelectedItem == null)
+            {
+                return;
+            }
+            DisplayAlert("Items Selecionado: ", ((Pokemon)e.SelectedItem).Nome, "ok");
+            lstPokemon.SelectedItem = null;
+        }
+    }
+}
---
+++ ListaPokemon/Pokemon.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace ListaPokemon
+{
+    public class Pokemon
+    {
+        public Pokemon()
+        {
+        }
+        public String Imagem { get; set; }
+        public String Nome { get; set; }
+        public String Tipo { get; set; }
+
+    }
+}
